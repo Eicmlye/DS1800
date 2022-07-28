915f3b14bad5ca5fb8185242fa8243b4a96ef9cb
Eicmlye <536682885@qq.com>
2022-07-28 11:13:28 +0800

    fixed display standard

--- DS1800Sol/chpt_3/3_15.cpp
+++ DS1800Sol/chpt_3/3_15.cpp
@@ -1,5 +1,5 @@
 /*
-	S1 ��Ϊ�������, S2 ��Ϊ������������ S1 Ϊ��ʱ, ���ֱ����ջ S1 ����; �� S1 �ǿ�, �Ƚ� S1 ��Ԫ�س�ջ��������ջ�� S2, �ٽ�Ҫ��ӵ�Ԫ����ջ S1���� S2 �ǿ�ʱ, ����ֱ�ӳ�ջ S2 ����; �� S2 Ϊ��, �Ƚ� S1 �е�Ԫ�س�ջ��������ջ�� S2, �ٽ� S2 ��ջһ�μ���. ��֤����Ϊ�ռ���֤����ջ��Ϊ��. 
+	S1 作为入队容器, S2 作为出队容器。当 S1 为空时, 入队直接入栈 S1 即可; 当 S1 非空, 先将 S1 的元素出栈并依次入栈到 S2, 再将要入队的元素入栈 S1。当 S2 非空时, 出队直接出栈 S2 即可; 当 S2 为空, 先将 S1 中的元素出栈并依次入栈到 S2, 再将 S2 出栈一次即可. 验证队列为空即验证两个栈均为空.
 */
 
 #include <iostream>
@@ -7,28 +7,28 @@
 
 void enqueue(DStkQue& que, const int x)
 {
-	if (que.stkIn_->top_ != que.stkIn_->size_) { // stkIn not full; 
-		que.stkIn_->PUSH(x); 
+	if (que.stkIn_->top_ != que.stkIn_->size_) { // stkIn not full;
+		que.stkIn_->PUSH(x);
 	}
-	else if (que.stkOut_->Sempty()) { // stkIn full, stkOut empty; 
-		int cache = 0; 
+	else if (que.stkOut_->Sempty()) { // stkIn full, stkOut empty;
+		int cache = 0;
 
 		while (que.stkIn_->top_ != 0) {
-			que.stkIn_->POP(cache); 
-			que.stkOut_->PUSH(cache); 
+			que.stkIn_->POP(cache);
+			que.stkOut_->PUSH(cache);
 		}
-		que.stkIn_->PUSH(x); 
+		que.stkIn_->PUSH(x);
 	}
-	else { // unable to enqueue; 
-		throw std::bad_alloc(); 
+	else { // unable to enqueue;
+		throw std::bad_alloc();
 	}
 }
 void dequeue(DStkQue& que, int& x)
 {
-	if (!que.stkOut_->Sempty()) { // stkOut nonempty; 
-		que.stkOut_->POP(x); 
+	if (!que.stkOut_->Sempty()) { // stkOut nonempty;
+		que.stkOut_->POP(x);
 	}
-	else if (!que.stkIn_->Sempty()) {// stkOut empty, stkIn nonempty; 
+	else if (!que.stkIn_->Sempty()) {// stkOut empty, stkIn nonempty;
 		int cache = 0;
 
 		while (que.stkIn_->top_ != 0) {
@@ -38,11 +38,11 @@ void dequeue(DStkQue& que, int& x)
 
 		que.stkOut_->POP(x);
 	}
-	else { // there is nothing to pop; 
-		throw std::bad_alloc(); 
+	else { // there is nothing to pop;
+		throw std::bad_alloc();
 	}
 }
 bool queue_empty(const DStkQue que)
 {
-	return (que.stkIn_->Sempty() && que.stkOut_->Sempty()); 
+	return (que.stkIn_->Sempty() && que.stkOut_->Sempty());
 }
