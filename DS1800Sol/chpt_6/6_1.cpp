050c44ca5338f331bf897cea1e2614f04a9dc114
Eicmlye <536682885@qq.com>
2022-08-08 12:45:28 +0800

    init. chpt 6

---
+++ DS1800Sol/chpt_6/6_1.cpp
@@ -0,0 +1,13 @@
+/*
+	只需求出各叶节点的带权路径长度相加即可. 考虑层序遍历该树, 遍历到叶节点时, 计算其带权路径长度, 并加至总 WPL 中. 
+*/
+
+#include <iostream>
+#include "6_1.h"
+
+unsigned int getWPL(BiTree tree, unsigned int WPL)
+{
+
+
+	return WPL; 
+}
---
+++ DS1800Sol/chpt_6/6_1.h
@@ -0,0 +1,14 @@
+#ifndef DS_6_1_H_
+#define DS_6_1_H_
+
+#include "chpt_tag.h"
+
+typedef struct TreeNode {
+	unsigned int weight = 0; 
+	TreeNode* left = nullptr; 
+	TreeNode* right = nullptr; 
+} TreeNode, *BiTree;
+
+unsigned int getWPL(BiTree tree, unsigned int WPL = 0); 
+
+#endif
