356db7ce47b48ef5a283e36ba294b63ed157e421
Eicmlye <536682885@qq.com>
2022-08-15 11:23:08 +0800

    new 6_10; init 6_11

--- DS1800Sol/chpt_6/6_10.h
+++ DS1800Sol/chpt_6/6_10.h
@@ -1,8 +1,18 @@
-#ifndef DS_6__H_
-#define DS_6__H_
+#ifndef DS_6_10_H_
+#define DS_6_10_H_
 
 #include "chpt_tag.h"
 
+typedef struct TreeNode {
+	int data = 0;
+	TreeNode* left = nullptr;
+	TreeNode* right = nullptr;
+} TreeNode, *BiTree;
 
+BiTree buildBiTree(size_t level = 0); 
+
+char** btree2Arr(BiTree tree, size_t& h); 
+
+BiTree levBuildTree(char** arr, size_t h); 
 
 #endif
